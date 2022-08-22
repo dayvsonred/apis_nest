32e79e0b9af82a709fd02e2c11b6da2269170334
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-08-22 19:57:50 -0300

    curd ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonController } from './person/person.controller';
 import { PersonService } from './person/person.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb+srv://outros:Z90TJtXpUdkkZP6b@cluster0.muxczfl.mongodb.net/test?authSource=admin&replicaSet=atlas-is65td-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+  ],
   controllers: [AppController, PersonController],
   providers: [AppService, PersonService],
 })
